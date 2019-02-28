981e309773e4564ab9a5c5780165ee0445b2d371
Victor Lava <hello@victorlava.com>
2019-02-28 11:48:16 +0200

    post forma, validacija su regex, funkcijos deklaracija

--- index.php
+++ index.php
@@ -1,3 +1,46 @@
+<?php
+
+// Typehint: nusako kintamojo tipa/tipus
+function isValidEmail(string $email) : bool {
+  $regex = '/^(?!(?:(?:\x22?\x5C[\x00-\x7E]\x22?)|(?:\x22?[^\x5C\x22]\x22?)){255,})(?!(?:(?:\x22?\x5C[\x00-\x7E]\x22?)|(?:\x22?[^\x5C\x22]\x22?)){65,}@)(?:(?:[\x21\x23-\x27\x2A\x2B\x2D\x2F-\x39\x3D\x3F\x5E-\x7E]+)|(?:\x22(?:[\x01-\x08\x0B\x0C\x0E-\x1F\x21\x23-\x5B\x5D-\x7F]|(?:\x5C[\x00-\x7F]))*\x22))(?:\.(?:(?:[\x21\x23-\x27\x2A\x2B\x2D\x2F-\x39\x3D\x3F\x5E-\x7E]+)|(?:\x22(?:[\x01-\x08\x0B\x0C\x0E-\x1F\x21\x23-\x5B\x5D-\x7F]|(?:\x5C[\x00-\x7F]))*\x22)))*@(?:(?:(?!.*[^.]{64,})(?:(?:(?:xn--)?[a-z0-9]+(?:-[a-z0-9]+)*\.){1,126}){1,}(?:(?:[a-z][a-z0-9]*)|(?:(?:xn--)[a-z0-9]+))(?:-[a-z0-9]+)*)|(?:\[(?:(?:IPv6:(?:(?:[a-f0-9]{1,4}(?::[a-f0-9]{1,4}){7})|(?:(?!(?:.*[a-f0-9][:\]]){7,})(?:[a-f0-9]{1,4}(?::[a-f0-9]{1,4}){0,5})?::(?:[a-f0-9]{1,4}(?::[a-f0-9]{1,4}){0,5})?)))|(?:(?:IPv6:(?:(?:[a-f0-9]{1,4}(?::[a-f0-9]{1,4}){5}:)|(?:(?!(?:.*[a-f0-9]:){5,})(?:[a-f0-9]{1,4}(?::[a-f0-9]{1,4}){0,3})?::(?:[a-f0-9]{1,4}(?::[a-f0-9]{1,4}){0,3}:)?)))?(?:(?:25[0-5])|(?:2[0-4][0-9])|(?:1[0-9]{2})|(?:[1-9]?[0-9]))(?:\.(?:(?:25[0-5])|(?:2[0-4][0-9])|(?:1[0-9]{2})|(?:[1-9]?[0-9]))){3}))\]))$/iD';
+  // $result = false;
+
+  preg_match(   $regex,
+                $email,
+                $emailArray,
+                PREG_OFFSET_CAPTURE,
+                0);
+
+  if(count($emailArray) === 0) {
+    $result = true;
+  }
+
+  // Ternary if/else
+  return (count($emailArray) === 0) ? false : true;
+}
+
+
+
+
+  // print_R($_GET);
+  // print_R($_POST);
+  // if()
+
+if(count($_POST) > 0) { // Jei masyve yra daugiau nei 0 elementu, vadinasi forma buvo išsiųsta
+    // echo "issiunte";
+
+    if(empty($_POST['name'])) {
+      echo "Iveskite varda.";
+    }
+
+    // Galima ir taip parašyti
+    // if(isValidEmail($_POST['email']) === false)
+    if(!isValidEmail($_POST['email'])) {
+      echo "Įveskite teisingą el. pašto adresą.";
+    }
+
+}
+?>
 <!DOCTYPE html>
 <html lang="en" dir="ltr">
   <head>
@@ -7,50 +50,21 @@
   <body>
     <h1>Sveiki</h1>
 
-    <?php
-      // $ kintamojo deklaracija
-      $vardas = "Viktoras"; // String tipo
-      $pavarde = "Lava";
-      $skaicius = 5;
-      $float = 5.21;
-      $array = [5, 10]; // indeksinis masyvas
-      $coordinates = [
-                      'x' => 5,
-                      'y' => 10,
-                      'z' => 5.2
-                    ]; // associatyvinis masyvas
-
-      // echo $array;
-      echo "<pre>";
-      print_R($array); // debuginimui
-      echo "</pre>";
-
-      for ($i = 0; $i < count($array); $i++) {
-        echo $array[$i] . "<br>";
-      }
-
-      // $coordinates['x'] =
-
-
-      echo "<pre>";
-      print_R($coordinates);
-      echo "</pre>";
-
-      foreach ($coordinates as $key => $value) {
-        echo "key: $key value: $value <br>";
-      }
-
-      // var_dump($arrayAssoc); // debuginimui, daugiau informacijos
-
-      echo("sveiki, $vardas");
-      echo 'sveiki,'.' '.$vardas; // sujungia eilutes, panasiai JS +
-      echo 'sveiki, ' . $vardas;
-      echo "${vardas} Hello world";
-      echo '4' . '5';
-      echo '4' + '5';
-
-
-      // echo sdf
-    ?>
+    <!--  -->
+    <form class="" action="/" method="post" novalidate>
+      <div class="">
+        <label for="">Name</label>
+        <input type="text" name="name" value="">
+      </div>
+     <div class="">
+      <label for="">E-mail</label>
+      <input type="email" name="email" value="">
+     </div>
+    <button type="submit">Send</button>
+    </form>
+    <footer>
+      All rights reserved.
+      <?php echo '©' . date('Y'); ?>
+    </footer>
   </body>
 </html>
